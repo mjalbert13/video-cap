f8027ebf1ba617b878814996e66e997f28825f0a
mjalbert13 <mjalbert13@gmail.com>
2020-02-20 14:42:04 -0500

    cleaning up code

--- capture.py
+++ capture.py
@@ -21,7 +21,7 @@ while True:
 
     delta_frame = cv2.absdiff(first_frame, gray)
     thresh_frame = cv2.threshold(delta_frame, 30,255, cv2.THRESH_BINARY)[1]
-    thresh_frame = cv2.dilate(thresh_frame,None, iterations=2)
+    thresh_frame = cv2.dilate(thresh_frame,None, iterations=3)
 
     (cnts,_) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
